9542625ddbab2176dd756d68bbb9a53dd52e16b4
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2018-05-29 11:50:49 -0400

    push

---
+++ user.c
@@ -0,0 +1,18 @@
+/*
+ * user.c
+ *
+ *  Created on: May 29, 2018
+ *      Author: Tech-Laptop
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL, "Goodbye, cruel world!", "Note", MB_OK);
+    return 0;
+}
